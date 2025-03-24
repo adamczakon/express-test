8121f93d869227b0fb5db2668edc146b89ab99ac
adam-czakon <a.czakon@digitalgateways.tech>
2025-03-24 13:31:47 +0100

    commit

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
+// app.use(cors());
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
