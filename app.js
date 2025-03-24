e8f6337446b740cf51fcc4c704f4eb847e425dc9
adam-czakon <a.czakon@digitalgateways.tech>
2025-03-24 13:26:32 +0100

    ADD CORS and cookies

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 8000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -9,6 +18,21 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("About route 🎉 ");
+});
+
+// Routes
+app.get("/set-cookie", (req, res) => {
+  res.cookie("test-cookie", "hello-world", {
+    httpOnly: true,
+    secure: true, // Set to true for HTTPS
+    sameSite: "None",
+  });
+  res.json({ message: "Cookie has been set!" });
+});
+
+app.get("/get-cookie", (req, res) => {
+  const cookie = req.cookies["test-cookie"];
+  res.json({ cookie });
 });
 
 app.listen(PORT, () => {
